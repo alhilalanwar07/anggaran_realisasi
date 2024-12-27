e6fefb6a00a3e167e8a89322adc0907379b3b7d1
Alhilal Anwar <alhilalanwar07@gmail.com>
2024-12-27 11:11:09 +0800

    update

--- resources/views/livewire/admin/laporan.blade.php
+++ resources/views/livewire/admin/laporan.blade.php
@@ -569,7 +569,7 @@ new class extends Component {
                 </div>
             </div>
         </div>
-        {{-- <div class="card">
+        <div class="card">
             <div class="card-header collapsed" id="headingTwo" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                 <div class="span-title">
                     Tabel Laporan
@@ -620,8 +620,8 @@ new class extends Component {
                     </div>
                 </div>
             </div>
-        </div> --}}
-        <div class="card">
+        </div>
+        {{-- <div class="card">
             <div class="card-header collapsed" id="headingThree" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                 <div class="span-title">
                     Lorem Ipsum #3
@@ -633,7 +633,7 @@ new class extends Component {
                     Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
 
     <script>
